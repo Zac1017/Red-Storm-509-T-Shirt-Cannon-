8c9ec86f7b8cac2825c3e43d51fdc65eb4006439
Zac1017 <9964262@bedfordnhk12.net>
2026-05-27 18:35:38 -0400

    Added Network Tables to TurretYamsTest, tested simulation. Values of network table are not passing into Advantage Scope or Robot Simulation

--- src/main/java/frc/robot/subsystems/TurretYAMSTest.java
+++ src/main/java/frc/robot/subsystems/TurretYAMSTest.java
@@ -1,72 +1,85 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import static edu.wpi.first.units.Units.Amps;
-// import static edu.wpi.first.units.Units.Degrees;
-// import static edu.wpi.first.units.Units.DegreesPerSecond;
-// import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
-// import static edu.wpi.first.units.Units.Meters;
-// import static edu.wpi.first.units.Units.Pounds;
-// import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.Seconds;
 
-// import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.TalonFX;
 
-// import edu.wpi.first.math.system.plant.DCMotor;
-// import edu.wpi.first.units.measure.Angle;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import yams.gearing.GearBox;
-// import yams.gearing.MechanismGearing;
-// import yams.mechanisms.config.PivotConfig;
-// import yams.mechanisms.positional.Pivot;
-// import yams.motorcontrollers.SmartMotorControllerConfig;
-// import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
-// import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
-// import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
-// import yams.motorcontrollers.remote.TalonFXWrapper;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.PivotConfig;
+import yams.mechanisms.positional.Pivot;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
 
-// public class TurretYAMSTest extends SubsystemBase {
+public class TurretYAMSTest extends SubsystemBase {
     
-//     private final TalonFX kRotationMotor = new TalonFX(0);
+    private final TalonFX kRotationMotor = new TalonFX(0);
 
-//     private final SmartMotorControllerConfig rotationMotorConfig = new SmartMotorControllerConfig()
-//         .withControlMode(ControlMode.CLOSED_LOOP)
-//         .withClosedLoopController(4, 0, 0, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
-//         .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-//         .withIdleMode(MotorMode.BRAKE)
-//         .withMotorInverted(false)
-//         .withTelemetry("TurretMotor", TelemetryVerbosity.HIGH)
-//         .withStatorCurrentLimit(Amps.of(40))
-//         .withClosedLoopRampRate(Seconds.of(0.25))
-//         .withOpenLoopRampRate(Seconds.of(0.25));
+    private final SmartMotorControllerConfig rotationMotorConfig = new SmartMotorControllerConfig()
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withClosedLoopController(4, 0, 0, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
+        .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+        .withIdleMode(MotorMode.BRAKE)
+        .withMotorInverted(false)
+        .withTelemetry("TurretMotor", TelemetryVerbosity.HIGH)
+        .withStatorCurrentLimit(Amps.of(40))
+        .withClosedLoopRampRate(Seconds.of(0.25))
+        .withOpenLoopRampRate(Seconds.of(0.25));
 
-//     private final TalonFXWrapper rotationMotorSMC = new TalonFXWrapper(kRotationMotor, DCMotor.getNEO(1), rotationMotorConfig);
+    private final TalonFXWrapper rotationMotorSMC = new TalonFXWrapper(kRotationMotor, DCMotor.getNEO(1), rotationMotorConfig);
 
-//     private final PivotConfig turretConfig = new PivotConfig(rotationMotorSMC)
-//         .withStartingPosition(Degrees.of(0))
-//         .withWrapping(Degrees.of(0), Degrees.of(360))
-//         .withHardLimit(Degrees.of(0), Degrees.of(360))
-//         .withTelemetry("TurretMech", TelemetryVerbosity.HIGH)
-//         .withMOI(Meters.of(0.25), Pounds.of(4));
+    private final PivotConfig turretConfig = new PivotConfig(rotationMotorSMC)
+        .withStartingPosition(Degrees.of(0))
+        .withWrapping(Degrees.of(0), Degrees.of(360))
+        .withHardLimit(Degrees.of(0), Degrees.of(360))
+        .withTelemetry("TurretMech", TelemetryVerbosity.HIGH)
+        .withMOI(Meters.of(0.25), Pounds.of(4));
 
-//     private final Pivot turretPivot = new Pivot(turretConfig);
-        
-//     public TurretYAMSTest() {
+    private final Pivot turretPivot = new Pivot(turretConfig);
+    
+    private final NetworkTable turretTable = NetworkTableInstance.getDefault().getTable("Turret");
 
-//     }
+    private final NetworkTableEntry turretAngle = turretTable.getEntry("angle");
 
-//     public Command setPivotAngle(Angle angle) {
-//         return turretPivot.setAngle(angle);
-//     }
-//     public Angle getAngle() {
-//         return turretPivot.getAngle();
-//     }
+    public TurretYAMSTest() {
 
-//     @Override
-//     public void periodic() {
-//         turretPivot.updateTelemetry();
-//     }
-//     @Override
-//     public void simulationPeriodic() {
-//         turretPivot.simIterate();
-//     }
-// }
+    }
+
+    public Command setPivotAngle(Angle angle) {
+        return turretPivot.setAngle(angle);
+    }
+    public Angle getAngle() {
+        return turretPivot.getAngle();
+    }
+
+    public Double convertAngleToDouble() {
+        return Double.valueOf(((getAngle()).toString()));
+    }
+
+    @Override
+    public void periodic() {
+        turretAngle.getDouble(convertAngleToDouble());
+        turretPivot.updateTelemetry();
+    }
+    
+    @Override
+    public void simulationPeriodic() {
+        turretPivot.simIterate();
+    }
+}
